4fbf8dfe70235fbb80bf2b39e7b9eb89065a7869
kkeerth5 <kkeerth5@10.91.197.81>
2026-02-11 11:25:15 +0530

    Commit

---
+++ src/main/java/com/inventory/order/repository/CustomersRepository.java
@@ -0,0 +1,16 @@
+//package com.inventory.order.repository;
+//
+//public class CustomersRepository {
+//
+//}
+
+package com.inventory.order.repository;
+ 
+import com.inventory.order.entity.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+import java.util.Optional;
+ 
+public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+    Optional<Customers> findByEmailAddress(String emailAddress);
+}
